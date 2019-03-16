ce1083be1a1c0b458f976cf0beab28bbc093f3fe
Sun Yuan Hong <sunyuan9@teach.cs.toronto.edu>
2019-03-16 14:27:10 -0400

    1

--- q3.sql
+++ q3.sql
@@ -4,11 +4,17 @@ DROP TABLE IF EXISTS q3 CASCADE;
 DROP VIEW IF EXISTS WinningElections CASCADE;
 DROP VIEW IF EXISTS Winners CASCADE;
 DROP VIEW IF EXISTS NumElecWins CASCADE;
+DROP VIEW IF EXISTS PartyNeverWon CASCADE;
+DROP VIEW IF EXISTS PartyCounts CASCADE;
+DROP VIEW IF EXISTS PartyCountsCountry CASCADE;
+DROP VIEW IF EXISTS CountryAvg CASCADE;
 DROP VIEW IF EXISTS DesiredParties CASCADE;
 DROP VIEW IF EXISTS DesPartiesInfo CASCADE;
+DROP VIEW IF EXISTS DesPartiesInfoFamily CASCADE;
 DROP VIEW IF EXISTS MostRecDate CASCADE;
 DROP VIEW IF EXISTS MostRecentWonElec CASCADE;
 DROP VIEW IF EXISTS res CASCADE;
+
 
 CREATE TABLE q3(
 	countryName VARCHAR(100),
@@ -29,33 +35,57 @@ CREATE VIEW WinningElections AS
 CREATE VIEW Winners AS
 	--returns the party id of the party who won the election with election_id on date election e_date
 	SELECT WinningElections.election_id eid, party_id, election.e_date edate
-	FROM WinningElections , election_result,election 
+	FROM WinningElections, election_result,election 
 
 	WHERE WinningElections.election_id = election_result.election_id 
 	and WinningElections.maxvotes = election_result.votes and
 	WinningElections.election_id = election.id;
 
+
 CREATE VIEW NumElecWins AS
-	--returns the number of elections won by the party winners as well as their ids 
 	SELECT party_id, count(*) Ws
 	FROM Winners
 	GROUP BY party_id;
 
+
+CREATE VIEW PartyNeverWon AS
+	SELECT id as party_id, 0 as ws
+	FROM party
+	WHERE id not in (SELECT party_id FROM NumElecWins);
+
+
+CREATE VIEW PartyCounts AS
+	(SELECT * FROM PartyNeverWon) UNION (SELECT * FROM NumElecWins);
+
+
+CREATE VIEW PartyCountsCountry AS 
+	SELECT party_id, party.country_id, PartyCounts.ws
+	FROM PartyCounts, party
+	WHERE PartyCounts.party_id = party.id;
+
+
+CREATE VIEW CountryAvg AS
+	SELECT country_id, avg(ws) as ws
+	FROM PartyCountsCountry
+	GROUP BY country_id;
+
+
 CREATE VIEW DesiredParties AS
-	-- returns the party ids and elections wins of the parties who won more than three times the average number of elections won by the partiesin the same country
-	SELECT A1.party_id party_id, A1.Ws Wins
-	FROM NumElecWins A1
-	WHERE A1.Ws > (
-		SELECT 3*AVG(coalesce(NumElecWins.Ws,0))
-		FROM NumElecWins, party,country
-		WHERE NumElecWins.party_id = party.id and party.country_id = country.id
-		GROUP BY country.id);
+	SELECT PartyCountsCountry.party_id, PartyCountsCountry.country_id, PartyCountsCountry.ws
+	FROM PartyCountsCountry, CountryAvg
+	WHERE PartyCountsCountry.country_id = CountryAvg.country_id and PartyCountsCountry.ws > 3*CountryAvg.ws;
+
 
 CREATE VIEW DesPartiesInfo AS
 	--returns all desired party information such as party name, party family etc.
-	SELECT party.name ParName, DesiredParties.party_id party_id, country.name CName, party_family.family Fam, coalesce(Wins,0) Ws
-	FROM party, DesiredParties, country, party_family
-	WHERE DesiredParties.party_id = party.id and DesiredParties.party_id = party_family.party_id and country.id = party.country_id;
+	SELECT party.name ParName, DesiredParties.party_id party_id, country.name as country, DesiredParties.Ws
+	FROM party, DesiredParties, country
+	WHERE DesiredParties.party_id = party.id and party.country_id = country.id;
+
+
+CREATE VIEW DesPartiesInfoFamily AS
+	select DesPartiesInfo.ParName, DesPartiesInfo.party_id, DesPartiesInfo.country, party_family.family as Fam, DesPartiesInfo.Ws
+	from DesPartiesInfo left join party_family on DesPartiesInfo.party_id = party_family.party_id;
 
 
 CREATE VIEW MostRecDate AS
@@ -64,16 +94,16 @@ CREATE VIEW MostRecDate AS
 	FROM Winners
 	GROUP BY party_id;
 
-CREATE VIEW MostRecentWonElec AS
 
-	SELECT Winners.party_id party_id, extract(year FROM edate)::int yer, eid
+CREATE VIEW MostRecentWonElec AS
+	SELECT Winners.party_id party_id, extract(year FROM Winners.edate)::int yer, Winners.eid
 	FROM Winners, MostRecDate 
-	WHERE Winners.party_id = MostRecDate.party_id and edate = mostrecdate;
+	WHERE Winners.party_id = MostRecDate.party_id and Winners.edate = MostRecDate.mostrecdate;
 
 
 CREATE VIEW res AS
-	SELECT DES.CName countryName, DES.ParName partyName, DES.Fam partyFamily, DES.Ws wonElections, MR.eid mostRecentlyWonElectionId, MR.yer mostRecentlyWonElectionYear  
-	FROM MostRecentWonElec MR, DesPartiesInfo DES 
-	WHERE DES.party_id = MR.party_id;
+	SELECT DP.country countryName, DP.ParName partyName, DP.Fam partyFamily, DP.Ws wonElections, MR.eid mostRecentlyWonElectionId, MR.yer mostRecentlyWonElectionYear  
+	FROM MostRecentWonElec MR, DesPartiesInfoFamily DP
+	WHERE MR.party_id = DP.party_id;
 
 insert into q3 (SELECT * FROM res)
